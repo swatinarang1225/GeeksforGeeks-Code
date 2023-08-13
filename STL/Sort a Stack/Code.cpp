9a664f7f5a8e933551e2c52fc907abde304377b8
Swati Narang <swatinarang1225@gmail.com>
2023-08-13 17:26:33 +0530

    Create Code.cpp

---
+++ STL/Sort a Stack/Code.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   //Your code here
+   vector<int> v1;
+   while(!s.empty()){
+       v1.push_back(s.top());
+       s.pop();
+   }
+    
+   std::sort(v1.begin(), v1.end());
+   
+   for (int i = 0; i <v1.size(); i++){
+       s.push(v1[i]);
+   }
+       
+   
+   
+}
