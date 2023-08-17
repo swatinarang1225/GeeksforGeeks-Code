be7150574472b3de7a60c3df15ca59e2b2b221b7
Swati Narang <swatinarang1225@gmail.com>
2023-08-17 18:29:17 +0530

    Create Code.cpp

---
+++ STL/Pair Sum in Vector/Code.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void sum(vector<pair<int,int>>v);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*the function contains vector of pairs*/
+//Complete this function to print the sum
+void sum(vector<pair<int,int>>v)
+{
+    long long sum=0;
+    //Your code here
+    vector<pair<int,int>>::iterator it1;
+    for(it1 = v.begin(); it1!=v.end(); it1++){
+        sum = sum + it1->second;
+    }
+    cout<<sum;
+    cout<<endl;
+}
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<pair<int,int>>v; //Making a vector that holds pairs
+	    for(int i=0;i<n;i++)
+	    {
+	        int x,y;
+	        cin>>x>>y;
+	        v.push_back(make_pair(x,y)); //Pushing a pair in the vector
+	    } 
+	    
+	   sum(v); //The sum function
+	   
+	}
+	return 0;
+}
+
+// } Driver Code Ends
