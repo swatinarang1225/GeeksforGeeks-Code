261e81e1151c5154fc915b313968ae0a2cc55334
Swati Narang <swatinarang1225@gmail.com>
2023-08-18 15:18:33 +0530

    Create Code.cpp

---
+++ STL/Pattern Searching/Code.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+str: string in which pattern we have to find pattern.
+text: pattern needs to searched.
+*/
+
+bool searchPattern(string str, string pat)
+{
+    // your code here
+    
+    int found = str.find(pat);
+    if(found != -1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    
+    while(t--)
+    {
+        string str, pat;
+        cin>>str;
+        cin>>pat;
+        
+        if(searchPattern(str, pat) == true)
+            cout << "Present" <<endl;
+        else cout << "Not present" <<endl;
+    }
+}
+
+
+// } Driver Code Ends
