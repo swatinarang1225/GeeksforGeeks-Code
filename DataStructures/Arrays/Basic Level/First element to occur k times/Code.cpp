3bb2f4dfcb1edcd6079af67ede20cefdc81340d5
Swati Narang <swatinarang1225@gmail.com>
2023-08-27 18:37:23 +0530

    Create Code.cpp

---
+++ DataStructures/Arrays/Basic Level/First element to occur k times/Code.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int a[], int n, int k)
+    {   
+        int out;
+        map<int,int> m1;
+        for(int i = 0; i<n;i++){
+            m1[a[i]]++;
+            if(m1[a[i]] == k){
+                return a[i];
+            }
+                
+            
+        }
+        return -1;
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(a, n, k)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
