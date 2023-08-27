06f33271c27b9e618bed8cd49f6ba69d777aaecc
Swati Narang <swatinarang1225@gmail.com>
2023-08-27 18:43:59 +0530

    Create Code.cpp

---
+++ DataStructures/Arrays/Basic Level/Reverse a String/Code.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        string str1;
+        for(int i = str.length()-1; i>=0; i--){
+            str1 += str[i];
+        }
+        return str1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
