02de17552d118eb37ee0a7a9317f6d376c1bda26
Swati Narang <swatinarang1225@gmail.com>
2023-08-29 16:37:53 +0530

    Create Code.cpp

---
+++ DataStructures/Arrays/Easy Level/Rotate Array/Code.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void rotateArr(int arr[], int d, int n){
+        // code here
+        while(d>n){
+                d = d%n;
+            }
+        vector<int> v1;
+        for(int i = d; i<n; i++){
+            v1.push_back(arr[i]);
+        }
+        for(int i = 0; i<d; i++){
+            v1.push_back(arr[i]);
+        }
+        
+        for(int i =0; i<v1.size(); i++){
+            arr[i] = v1[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
