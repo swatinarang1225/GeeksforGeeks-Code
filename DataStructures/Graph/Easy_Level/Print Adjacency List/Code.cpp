884f0bcfb290084fb532b81df41467765d5054ea
Swati Narang <swatinarang1225@gmail.com>
2023-09-05 12:20:50 +0530

    Create Code.cpp

---
+++ DataStructures/Graph/Easy_Level/Print Adjacency List/Code.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
+        vector<vector<int>> v1(V);
+        unordered_map<int, vector<int>> adj_map;
+        
+        for( auto x: edges){
+            int u = x.first;
+            int v = x.second;
+            
+            adj_map[u].push_back(v);
+            adj_map[v].push_back(u);
+        }
+        
+        for(int i = 0; i<V; i++){
+            v1[i] = adj_map[i];
+        }
+        
+        return v1;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int,int>>edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u,v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
