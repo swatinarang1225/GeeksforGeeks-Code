e5195c800db34e8122246e5c562b6640dedcc8fa
Swati Narang <swatinarang1225@gmail.com>
2024-04-06 00:11:16 +0530

    Create Code.cpp

---
+++ DataStructures/Arrays/Easy Level/Segregate 0s and 1s/Code.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        int zero_count = 0;
+        for(int i = 0;i<n;i++){
+            if(arr[i] == 0){
+                zero_count += 1;
+            }
+        }
+        
+        for(int i = 0; i<zero_count; i++){
+            arr[i] = 0;
+        }
+        for(int i = zero_count; i<n; i++){
+            arr[i] = 1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.segregate0and1(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
